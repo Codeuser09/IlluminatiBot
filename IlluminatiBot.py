31950a868075d446060b43be626330aa8bee2cd6
Codeuser09 <70903742+Codeuser09@users.noreply.github.com>
2023-12-08 14:11:28 +0000

    Add files via upload

---
+++ IlluminatiBot.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  4 11:09:30 2023
+
+@author: Simon
+"""
+
+import keyboard
+import time
+
+
+time.sleep(5)
+
+def UsePetDrop(Num):
+    time.sleep(1)
+    keyboard.press_and_release(Num)
+    time.sleep(1)
+    keyboard.press_and_release('F')
+    time.sleep(1)
+    keyboard.press_and_release('9')
+    time.sleep(1)
+    keyboard.press_and_release('Q')
+
+def sleep():
+    '''
+    This is stupid but it allows me to stop it every 1 second
+    '''
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(1)
+    
+        
+
+while True:
+    UsePetDrop('1')
+    UsePetDrop('2')
+    UsePetDrop('3')
+    UsePetDrop('4')
+    UsePetDrop('5')
+    UsePetDrop('6')
+    UsePetDrop('7')
+    UsePetDrop('8')
+    sleep()
+    
